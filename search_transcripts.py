82420c3f0d92f0b42b479a474b0bc935f6c9f7e0
Noel Reed <noelreed3@gmail.com>
2025-06-23 21:49:05 -0500

    Removed unused failing code at the end of search_transcripts.py

--- search_transcripts.py
+++ search_transcripts.py
@@ -29,7 +29,3 @@ if __name__ == "__main__":
         if term.lower() == 'q':
             break
         search(term)
-
-results = db.search_segments(term)
-# Each result should include the keyword (search term used)
-results = [(r.id, r.file, r.start, r.end, r.text, term) for r in results]
